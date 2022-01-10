575f4cc78bca825382e837898e5bd3891dacbf31
Philipp <alber.philipp@gmail.com>
2022-01-10 17:35:25 +0100

    fixed bug of index to position transform

--- pytest/test_agent.py
+++ pytest/test_agent.py
@@ -234,16 +234,37 @@ def test_hashingMove():
 
 
 def test_indexToPosition():
+
+    """
+        [R0    1     2     3     4     5   
+    C0  '00', '01', '02', '03', '04', '05',
+    1   '06', '07', '08', '09', '10', '11',
+    2   '12', '13', '14', '15', '16', '17',
+    3   '18', '19', '20', '21', '22', '23',
+    4   '24', '25', '26', '27', '28', '29',
+    5   '30', '31', '32', '33', '34', '35'
+        ]
+    """
+
+    agent.__init__()
+
     
     index = 0
-    position = agent.indexToPosition(index)
+    position = agent.getPositionOfIndex(index)
 
-    assert position == (0,0), "Index to position is wrong"
+    assert position == (0,0), "Index to position is wrong1"
 
-    index = 36
-    position = agent.indexToPosition(index)
+    index = 25
+    position = agent.getPositionOfIndex(index)
 
-    assert position == (5,5), "Index to position is wrong"
+    print(position)
+
+    assert position == (1,4), "Index to position is wrong2"
+
+    index = 35
+    position = agent.getPositionOfIndex(index)
+
+    assert position == (5,5), "Index to position is wrong3"
 
 
 
